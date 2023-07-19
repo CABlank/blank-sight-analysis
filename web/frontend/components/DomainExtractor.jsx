1d588846404dc861f9f0495a4bedfe0562ed0e43
CABlank <54187031+CABlank@users.noreply.github.com>
2023-07-19 13:50:29 +0800

    s

--- web/frontend/components/DomainExtractor.jsx
+++ web/frontend/components/DomainExtractor.jsx
@@ -1,34 +1,103 @@
-
-import React, { useState, useEffect } from 'react';
-import { useLocation } from 'react-router-dom';
+import { useState } from "react";
+import { Card, TextContainer, Text } from "@shopify/polaris";
+import { Toast } from "@shopify/app-bridge-react";
+import { useTranslation } from "react-i18next";
 import { useAppQuery, useAuthenticatedFetch } from "../hooks";
+import { useQuery } from '@apollo/client';
+import { gql } from 'graphql-tag';
 
-const DomainExtractor = () => {
-  const location = useLocation();
-  const fetch = useAuthenticatedFetch();
-  const [domain, setDomain] = useState('');
-
-  useEffect(() => {
-    const searchParams = new URLSearchParams(location.search);
-    const shop = searchParams.get('shop');
-    if (shop) {
-      fetch(`https://blank-sight-analysis-551f1a0e4152.herokuapp.com/api/shopify/${shop}`)
-        .then((response) => response.json())
-        .then((data) => {
-          setDomain(data.shop.myshopify_domain);
-        })
-        .catch((error) => {
-          console.error('Failed to fetch custom domain:', error);
-        });
+const GET_SHOP_PRIMARY_DOMAIN = gql`
+  query {
+    shop {
+      primaryDomain {
+        url
+        host
+      }
     }
-  }, [fetch, location.search]);
+  }
+`;
+
+export function ProductsCard() {
+  const emptyToastProps = { content: null };
+  const [isLoading, setIsLoading] = useState(true);
+  const [toastProps, setToastProps] = useState(emptyToastProps);
+  const fetch = useAuthenticatedFetch();
+  const { t } = useTranslation();
+  const productsCount = 5;
+
+  const {
+    data: productCountData,
+    refetch: refetchProductCount,
+    isLoading: isLoadingCount,
+    isRefetching: isRefetchingCount,
+  } = useAppQuery({
+    url: "/api/products/count",
+    reactQueryOptions: {
+      onSuccess: () => {
+        setIsLoading(false);
+      },
+    },
+  });
+
+  const { loading: domainLoading, error: domainError, data: domainData } = useQuery(GET_SHOP_PRIMARY_DOMAIN);
+
+  const toastMarkup = toastProps.content && !isRefetchingCount && (
+    <Toast {...toastProps} onDismiss={() => setToastProps(emptyToastProps)} />
+  );
+
+  const handlePopulate = async () => {
+    setIsLoading(true);
+    const response = await fetch("/api/products/create");
+
+    if (response.ok) {
+      await refetchProductCount();
+      setToastProps({
+        content: t("ProductsCard.productsCreatedToast", {
+          count: productsCount,
+        }),
+      });
+    } else {
+      setIsLoading(false);
+      setToastProps({
+        content: t("ProductsCard.errorCreatingProductsToast"),
+        error: true,
+      });
+    }
+  };
 
   return (
-    <div>
-      <h1>Store Domain</h1>
-      <p>{domain}</p>
-    </div>
+    <>
+      {toastMarkup}
+      <Card
+        title={t("ProductsCard.title")}
+        sectioned
+        primaryFooterAction={{
+          content: t("ProductsCard.populateProductsButton", {
+            count: productsCount,
+          }),
+          onAction: handlePopulate,
+          loading: isLoading,
+        }}
+      >
+        <TextContainer spacing="loose">
+          <p>{t("ProductsCard.description")}</p>
+          <Text as="h4" variant="headingMd">
+            {t("ProductsCard.totalProductsHeading")}
+            <Text variant="bodyMd" as="p" fontWeight="semibold">
+              {isLoadingCount ? "-" : productCountData.count}
+            </Text>
+          </Text>
+          {domainLoading && <p>Loading domain...</p>}
+          {domainError && <p>Error loading domain: {domainError.message}</p>}
+          {domainData && (
+            <div>
+              <h3>Shop's primary domain:</h3>
+              <p>URL: {domainData.shop.primaryDomain.url}</p>
+              <p>Host: {domainData.shop.primaryDomain.host}</p>
+            </div>
+          )}
+        </TextContainer>
+      </Card>
+    </>
   );
-};
-
-export default DomainExtractor;
+}
