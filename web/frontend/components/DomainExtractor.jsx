c6327bacdcc757999702c4b8e95c11795661c9ac
CABlank <54187031+CABlank@users.noreply.github.com>
2023-07-19 13:29:07 +0800

    Update DomainExtractor.jsx

--- web/frontend/components/DomainExtractor.jsx
+++ web/frontend/components/DomainExtractor.jsx
@@ -1,34 +1,27 @@
+
 import React, { useState, useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
-import { getSessionToken } from '@shopify/app-bridge-utils';
-import { useAppBridge } from '@shopify/app-bridge-react';
+import useAuthenticatedFetch from '../hooks/useAuthenticatedFetch';
 
 const DomainExtractor = () => {
   const location = useLocation();
-  const app = useAppBridge();
+  const fetch = useAuthenticatedFetch();
   const [domain, setDomain] = useState('');
 
   useEffect(() => {
     const searchParams = new URLSearchParams(location.search);
     const shop = searchParams.get('shop');
     if (shop) {
-      getSessionToken(app)
-        .then((token) => {
-          fetch(`https://blank-sight-analysis-551f1a0e4152.herokuapp.com/api/shopify/${shop}`, {
-            headers: {
-              'Authorization': `Bearer ${token}`,
-            },
-          })          
-            .then((response) => response.json())
-            .then((data) => {
-              setDomain(data.shop.myshopify_domain);
-            })
-            .catch((error) => {
-              console.error('Failed to fetch custom domain:', error);
-            });
+      fetch(`https://blank-sight-analysis-551f1a0e4152.herokuapp.com/api/shopify/${shop}`)
+        .then((response) => response.json())
+        .then((data) => {
+          setDomain(data.shop.myshopify_domain);
+        })
+        .catch((error) => {
+          console.error('Failed to fetch custom domain:', error);
         });
     }
-  }, [app, location.search]);
+  }, [fetch, location.search]);
 
   return (
     <div>
